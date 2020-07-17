9cdd2346fe6fe25e34f3ea34a4c8430a38f8706f
Sergey Zubov <serjio.zubov@gmail.com>
2020-07-17 14:35:27 +0300

    for check

--- app/src/main/java/com/szubov/android_hw_102/MainActivity.java
+++ app/src/main/java/com/szubov/android_hw_102/MainActivity.java
@@ -7,11 +7,11 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import androidx.annotation.NonNull;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -21,10 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private final String KEY2 = "key2";
     private SharedPreferences mListSharedPref;
     private final String LIST_TEXT = "list_text";
-    private List<Map<String,String>> mArrayList;
+    private List<Map<String,String>> mArrayList = new ArrayList<>();
     private BaseAdapter mListContentAdapter;
-    private ListView mListView;
-
+    private SwipeRefreshLayout mSwipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +34,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        mListView = findViewById(R.id.list);
+        ListView mListView = findViewById(R.id.list);
         mListSharedPref = getSharedPreferences("MyText", MODE_PRIVATE);
+        mSwipeRefresh = findViewById(R.id.swipeRefresh);
 
         if (!mListSharedPref.contains(getString(R.string.large_text))) {
             SharedPreferences.Editor myEditor = mListSharedPref.edit();
@@ -44,60 +44,50 @@ public class MainActivity extends AppCompatActivity {
             myEditor.apply();
         }
 
-        mArrayList = prepareContent();
+        prepareContent();
         mListContentAdapter = createAdapter(mArrayList);
         mListView.setAdapter(mListContentAdapter);
+
+        mSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                prepareContent();
+                mListContentAdapter.notifyDataSetChanged();
+                mSwipeRefresh.setRefreshing(false);
+            }
+        });
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedString = parent.getItemAtPosition(position).toString();
-
-                /*for (Map<String,String> list : mArrayList) {
-                    Iterator<Map.Entry<String,String>> maps = list.entrySet().iterator();
-                    StringBuilder selectedItem = new StringBuilder();
-                    while (maps.hasNext()) {
-                        Map.Entry<String, String> entry = maps.next();
-                        selectedItem.append(entry.getValue());
-                    }
-                    if (selectedItem.toString().equals(selectedString)) {
-                        mArrayList.remove(list);
-                    }
-                }*/
-                /*for (Map<String,String> list : mArrayList) {
-                    StringBuilder selectedItem = new StringBuilder();
-                    for (Map.Entry<String,String> map : list.entrySet()) {
-                        selectedItem.append(map.getValue());
-                    }
-                    if (selectedItem.toString().equals(selectedString)) {
-                        mArrayList.remove(list);
-                    }
-                }*/
-                /*for (Map<String,String> i : mArrayList) {
-                    if (i.containsValue(selectedString)) {
+                String mSelectedItem = ((TextView)view.findViewById(R.id.textView1)).
+                        getText().toString() + ((TextView)view.findViewById(R.id.textView2)).
+                        getText().toString();
+                for (int i = 0; i < mArrayList.size(); i++) {
+                    if (mSelectedItem.equals(mArrayList.get(i).get(KEY1) +
+                            mArrayList.get(i).get(KEY2))) {
                         mArrayList.remove(i);
+                        mListContentAdapter.notifyDataSetChanged();
+                        break;
                     }
-                }*/
-                mListContentAdapter.notifyDataSetChanged();
+                }
             }
         });
     }
-
 
     private BaseAdapter createAdapter(List<Map<String,String>> arrayList) {
         return new SimpleAdapter(this, arrayList, R.layout.list_item,
                 new String[]{KEY1,KEY2}, new int[]{R.id.textView1, R.id.textView2});
     }
 
-    private List<Map<String,String>> prepareContent() {
+    private void prepareContent() {
         String[] arrayContent = mListSharedPref.getString(LIST_TEXT,"").split("\n\n");
-        List<Map<String,String>> list = new ArrayList<>();
+        mArrayList.clear();
         for (String s : arrayContent) {
             Map<String, String> map = new HashMap<>();
             map.put(KEY1, s);
             map.put(KEY2, s.length() + "");
-            list.add(map);
+            mArrayList.add(map);
         }
-        return list;
     }
 }
